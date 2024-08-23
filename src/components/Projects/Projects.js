301a8d24d1ad1477c46b8a4636484d293a01e751
fredjaoko <95338158+fredjaoko@users.noreply.github.com>
2024-08-23 20:35:40 +0000

    github update

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -28,7 +28,7 @@ function Projects() {
               isBlog={false}
               title="🚕 Performing Data Analytics on NYC Taxi data using GCP and MageAI "
               description="The goal of this project is to perform data analytics on NYC Taxi Trip Records using various tools and technologies, including GCP Storage, Python, Compute Instance, Mage Data Pipeline Tool, BigQuery, and Looker Studio."
-              ghLink="https://github.com/freddyjaoko/NYCTaxi-Analytics-ETL-GCP"
+              ghLink="https://github.com/fredjaoko/NYC-Taxi-Analytics-GCP"
             />
           </Col>
 
@@ -38,7 +38,7 @@ function Projects() {
               isBlog={false}
               title="Employee Attrition Analysis Using SparkSQL on Databricks"
               description="Analysis of employee data using Apache Spark on Databricks to derive insights about customer attrition."
-              ghLink="https://github.com/freddyjaoko/Employee-attrition-analysis-using-sparkSQL-on-Databricks"
+              ghLink="https://github.com/fredjaoko/Employee-attrition-analysis-using-sparkSQL-on-Databricks"
             />
           </Col>
 
@@ -48,7 +48,7 @@ function Projects() {
               isBlog={false}
               title="Stock Market Prediction Using Daily News Headlines"
               description="This project is about predicting the BSE SENSEX index on the Mumbai stock exchange in India using daily news headlines. The stock market data of each day is collected from Yahoo finance. Combined both datasets to process and applied modeling techniques to get desired results. Different NLP techniques and machine learning models are used to address the problem defined."
-              ghLink="https://github.com/freddyjaoko/stock-market-prediction-using-daily-news-headlines"
+              ghLink="https://github.com/fredjaoko/stock-market-prediction-using-daily-news-headlines"
             />
           </Col>
 
@@ -58,7 +58,7 @@ function Projects() {
               isBlog={false}
               title="Insurance Fraud Detection"
               description="An end to end project using machine learning to detect fraud in insurance claims. "
-              ghLink="https://github.com/freddyjaoko/insurance-fraud-detection"
+              ghLink="https://github.com/fredjaoko/insurance-fraud-detection"
             />
           </Col>
 
@@ -68,7 +68,7 @@ function Projects() {
               isBlog={false}
               title="Back-end Engineering internship @ Lyft"
               description="This project is a virtual internship on forage by Lyft(a popular ridesharing company). In this project I implimented a backend system from the design stage upto implimentation and testing."
-              ghLink="https://github.com/freddyjaoko/lyft-backend-engineering-virtual-internship"
+              ghLink="https://github.com/fredjaoko/lyft-backend-engineering-virtual-internship"
                             
             />
           </Col>
@@ -79,7 +79,7 @@ function Projects() {
               isBlog={false}
               title="Data Science virtual Job simulation @ Boston Consulting group"
               description="In this virtual job simulation I undertook task as a Data science consultant at Boston consulting group consulting for a power and gas conmpany who was concerned about losing customers. I undertook exploratory data analysis, feature engineering and modelling to predict customer churn."
-              ghLink="https://github.com/freddyjaoko/BCG_job_simulation"
+              ghLink="https://github.com/fredjaoko/BCG_job_simulation"
             />
           </Col>
 
