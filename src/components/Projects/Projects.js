b8e7d7f28730dd4e6849700fe8ffa4a62a8e9c7a
fredjaoko <95338158+fredjaoko@users.noreply.github.com>
2024-10-16 06:37:17 +0000

    updates

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -28,7 +28,7 @@ function Projects() {
               isBlog={false}
               title="🚕 Performing Data Analytics on NYC Taxi data using GCP and MageAI "
               description="The goal of this project is to perform data analytics on NYC Taxi Trip Records using various tools and technologies, including GCP Storage, Python, Compute Instance, Mage Data Pipeline Tool, BigQuery, and Looker Studio."
-              ghLink="https://github.com/fredjaoko/NYC-Taxi-Analytics-GCP"
+              ghLink="https://github.com/freddyjaoko/NYC-Taxi-Analytics-GCP"
             />
           </Col>
 
@@ -38,7 +38,7 @@ function Projects() {
               isBlog={false}
               title="Employee Attrition Analysis Using SparkSQL on Databricks"
               description="Analysis of employee data using Apache Spark on Databricks to derive insights about customer attrition."
-              ghLink="https://github.com/fredjaoko/Employee-attrition-analysis-using-sparkSQL-on-Databricks"
+              ghLink="https://github.com/freddyjaoko/Employee-attrition-analysis-using-sparkSQL-on-Databricks"
             />
           </Col>
 
@@ -48,7 +48,7 @@ function Projects() {
               isBlog={false}
               title="Cricket Statistics Data Pipeline Project"
               description="This project showcases a robust data pipeline designed to retrieve cricket statistics using Python and the Cricbuzz API. The data is stored in Google Cloud Storage (GCS) in CSV format, where a Cloud Function is set up to trigger upon file upload. The Cloud Function launches a Dataflow job to load the data into BigQuery, enabling further analysis. The pipeline concludes with the creation of a Looker dashboard, providing real-time visual insights from the ingested cricket data. This project highlights skills in API integration, cloud storage, data processing, and visualization tools like Looker."
-              ghLink="https://github.com/fredjaoko/cricket-stat-data-engineering-project"
+              ghLink="https://github.com/freddyjaoko/cricket-stat-data-engineering-project"
             />
           </Col>
 
@@ -58,7 +58,7 @@ function Projects() {
               isBlog={false}
               title="Insurance Fraud Detection"
               description="An end to end project using machine learning to detect fraud in insurance claims. "
-              ghLink="https://github.com/fredjaoko/insurance-fraud-detection"
+              ghLink="https://github.com/freddyjaoko/insurance-fraud-detection"
             />
           </Col>
 
@@ -68,7 +68,7 @@ function Projects() {
               isBlog={false}
               title="Back-end Engineering internship @ Lyft"
               description="This project is a virtual internship on forage by Lyft(a popular ridesharing company). In this project I implimented a backend system from the design stage upto implimentation and testing."
-              ghLink="https://github.com/fredjaoko/lyft-backend-engineering-virtual-internship"
+              ghLink="https://github.com/freddyjaoko/lyft-backend-engineering-virtual-internship"
                             
             />
           </Col>
@@ -79,7 +79,7 @@ function Projects() {
               isBlog={false}
               title="Data Science virtual Job simulation @ Boston Consulting group"
               description="In this virtual job simulation I undertook task as a Data science consultant at Boston consulting group consulting for a power and gas conmpany who was concerned about losing customers. I undertook exploratory data analysis, feature engineering and modelling to predict customer churn."
-              ghLink="https://github.com/fredjaoko/BCG_job_simulation"
+              ghLink="https://github.com/freddyjaoko/BCG_job_simulation"
             />
           </Col>
 
